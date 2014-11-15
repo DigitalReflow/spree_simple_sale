78131a737cc6dcdd1bee12614534092f0fe9f53b
Dan Mitchell <dan@digitalreflow.co.uk>
2014-11-15 09:01:43 +0000

    update assets path to vendor

--- lib/generators/spree_simple_sales/install/install_generator.rb
+++ lib/generators/spree_simple_sales/install/install_generator.rb
@@ -5,13 +5,13 @@ module SpreeSimpleSales
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_simple_sales\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_simple_sales\n"
+        append_file 'app/vendor/assets/javascripts/store/all.js', "//= require store/spree_simple_sales\n"
+        append_file 'app/vendor/assets/javascripts/admin/all.js', "//= require admin/spree_simple_sales\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_simple_sales\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_simple_sales\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/vendor/assets/stylesheets/store/all.css', " *= require store/spree_simple_sales\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/vendor/assets/stylesheets/admin/all.css', " *= require admin/spree_simple_sales\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
