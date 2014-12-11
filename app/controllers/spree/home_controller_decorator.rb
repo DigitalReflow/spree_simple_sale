54ea5a9b34d0c67ee84caa631a406f2671a1a550
Dan Mitchell <dan@digitalreflow.co.uk>
2014-12-11 20:10:30 +0000

    update searcher method for pagination

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   HomeController.class_eval do
     def sale
-      @searcher = build_searcher({ sale: true })
+      @searcher = build_searcher({ sale: true, page: params[:page]})
       @products = @searcher.retrieve_products
       # @products = Product.joins(:variants_including_master).where('spree_variants.sale_price is not null').uniq
     end
